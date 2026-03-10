b4cbc9037296ad4ee67d74f6ed90a397e48e298d
MohDAdamSTTN <sithuthetnaing@hexcode.tech>
2026-03-11 06:33:22 +0700

    Fix libpdfium loading using pypdfium2.raw._LIB

--- pypdfium2_custom/__init__.py
+++ pypdfium2_custom/__init__.py
@@ -1,16 +1,15 @@
 import ctypes
-import pypdfium2
 import pypdfium2.raw as pdfium_raw
 
-# load the same libpdfium used by pypdfium2
-pdfium_lib = ctypes.CDLL(pdfium_raw.__file__.replace("__init__.py", "libpdfium.so"))
+# get the already-loaded pdfium library
+pdfium_lib = pdfium_raw._LIB
 
 # expose your function
 FPDFFormObj_RemoveObject = pdfium_lib.FPDFFormObj_RemoveObject
 
 FPDFFormObj_RemoveObject.argtypes = [
     ctypes.c_void_p,  # FPDF_FORMHANDLE
-    ctypes.c_void_p   # FPDF_PAGEOBJECT
+    ctypes.c_void_p,  # FPDF_PAGEOBJECT
 ]
 
 FPDFFormObj_RemoveObject.restype = None
